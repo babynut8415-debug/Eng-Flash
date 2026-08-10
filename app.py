f3233bf5e5d30035620c11bb55406cb28fdf7327
babynut8415-debug <babynut8415@gmail.com>
2026-08-10 23:08:23 +0900

    Fix formatting in user instruction message

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ with st.sidebar:
 # 메인 화면: 카드 퀴즈
 # ---------------------------------------------------------------------------
 if not st.session_state.cards:
-    st.info("왼쪽에서 "단어 : 뜻"을 직접 입력해 주세요. 👈")
+    st.info("왼쪽에서 [단어 : 뜻] 을 직접 입력해 주세요. 👈")
 else:
     cards = st.session_state.cards
     total = len(cards)
